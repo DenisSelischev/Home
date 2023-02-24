6afdcb6b71eca27f308411d1c7d2977632d2a6d3
Denis <Deni-SelischeV23@yandex.ru>
2023-02-24 21:36:06 +0300

    Solved 3 task

---
+++ task003/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int index = 1;
+  int length = cube.Length;
+  while (index < length)
+  {
+    cube[index] = Convert.ToInt32(Math.Pow(index, 3));
+    index++;
+  }
+}
+
+void Print(int[] coll)
+{
+  int count = coll.Length;
+  int index = 1;
+  while(index < count)
+  {
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[]number = new int[cube + 1];
+Cube(number);
+Print(number); 
